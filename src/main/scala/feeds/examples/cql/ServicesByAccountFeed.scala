a088ec4c0b8ac016457dbf3e32817bbf7829f95e
unknown <sourav.biswas@datastax.com>
2018-05-07 14:53:54 -0600

    iteration 1 - services by account

---
+++ src/main/scala/feeds/examples/cql/ServicesByAccountFeed.scala
@@ -0,0 +1,32 @@
+package feeds.examples.cql
+
+import java.util.Date
+
+import com.datastax.gatling.stress.core.BaseFeed
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.collection.JavaConverters._
+
+
+class ServicesByAccountFeed extends BaseFeed with LazyLogging {
+
+  def write = {
+    def rowData = getRowData
+
+    // optionally log out the data that was generated for future use
+    // logger.info("{},{},{}", Array(rowData.get("order_no"), rowData.get("cust_id"), rowData.get("email")))
+
+    Iterator.continually(rowData)
+  }
+
+
+  def getRowData = {
+    Map(
+      "account_number" -> faker.number().randomNumber(16, true), 
+      "service_type" -> faker.options.option("DATA","VOICE","VIDEO"),
+      "service_status" -> faker.options.option("ACTIVE","INACTIVE"),
+      "addition_timestamp" -> getRandomEpoch,
+      "class_of_service" -> faker.options.option("class_of_service_1","class_of_service_2","class_of_service_3")
+    )
+  }
+}
